ee77c5e8e29e43beb63be59598513ab5dcea6646
Harri-0409 <harriragunathan@gmail.com>
2026-04-06 16:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-let spreads = document.querySelectorAll(".spread");
 let current = 0;
+const spreads = document.querySelectorAll(".spread");
 
 function showPage(index) {
-  spreads.forEach((spread, i) => {
-    spread.classList.remove("active");
-  });
-
-  spreads[index].classList.add("active");
+    spreads.forEach((spread, i) => {
+        spread.classList.remove("active");
+        if (i === index) {
+            spread.classList.add("active");
+        }
+    });
 }
 
 function nextPage() {
-  if (current < spreads.length - 1) {
-    current++;
-    showPage(current);
-  }
+    if (current < spreads.length - 1) {
+        current++;
+        showPage(current);
+    }
 }
 
 function prevPage() {
-  if (current > 0) {
-    current--;
-    showPage(current);
-  }
+    if (current > 0) {
+        current--;
+        showPage(current);
+    }
 }
